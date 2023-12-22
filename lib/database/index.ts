1aab473ea79518c2c592865631b6fa51a91366aa
Gurupreeth Singh <igurupreeth@gmail.com>
2023-12-22 17:40:44 +0530

    first commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -9,6 +9,8 @@ export const connectToDatabase = async () => {
 
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
+
+
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
     dbName: 'evently',
     bufferCommands: false,
